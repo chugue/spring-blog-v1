1baeee90652e69e3e00458fc70238000990d52ec
AI_Nomads <chugue85@gmail.com>
2024-02-02 14:46:08 +0900

    version updated

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,12 +1,11 @@
 package shop.mtcoding.blog.user;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
+
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpSession;
 
 /*
  * 컨트롤러
